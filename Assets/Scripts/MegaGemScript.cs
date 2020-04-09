012c16aa10bfdbf2c64b17c401c1071bc0006b68
Abhiek187 <achaudhuri2011@yahoo.com>
2020-04-09 13:55:06 -0400

    Add mega gem

---
+++ Assets/Scripts/MegaGemScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MegaGemScript : MonoBehaviour
+{
+    private Transform parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Keep rotating the gem
+        parent.Rotate(0, 20 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "FPSController")
+        {
+            // Player touched the gem, remove from the scene
+            AudioSource.PlayClipAtPoint(parent.GetComponent<AudioSource>().clip, transform.position);
+            Destroy(parent.gameObject);
+        }
+    }
+}
