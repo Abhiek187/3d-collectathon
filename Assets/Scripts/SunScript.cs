f70ec6677ccd609d2033a49573cd17129a87e743
Abhiek187 <achaudhuri2011@yahoo.com>
2020-04-09 15:07:16 -0400

    Transition to nighttime once all gems are collected

--- Assets/Scripts/SunScript.cs
+++ Assets/Scripts/SunScript.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class SunScript : MonoBehaviour
 {
     [SerializeField] private Material nightSky;
+
+    private Transform gems;
     private float duration = 5f;
     private float intensity;
 
     // Start is called before the first frame update
     void Start()
     {
+        gems = GameObject.Find("Mega Gems").transform;
         intensity = GetComponent<Light>().intensity;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        // When all the mega gems have been collected, cue to ending
+        if (gems.childCount == 0)
+        {
+            NightTransition();
+        }
+    }
+
+    private void NightTransition()
     {
         if (intensity > 0)
         {
@@ -23,6 +35,7 @@ public class SunScript : MonoBehaviour
         }
         else if (intensity < 0)
         {
+            intensity = 0;
             Destroy(gameObject);
             RenderSettings.skybox = nightSky;
         }
