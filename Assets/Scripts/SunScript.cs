4a4bffa7cf36f97b95b0e0edabea0643187bfec0
Abhiek187 <achaudhuri2011@yahoo.com>
2020-04-09 14:52:13 -0400

    Add script to go from day to night

---
+++ Assets/Scripts/SunScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunScript : MonoBehaviour
+{
+    [SerializeField] private Material nightSky;
+    private float duration = 5f;
+    private float intensity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        intensity = GetComponent<Light>().intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (intensity > 0)
+        {
+            intensity -= Time.deltaTime / duration;
+        }
+        else if (intensity < 0)
+        {
+            Destroy(gameObject);
+            RenderSettings.skybox = nightSky;
+        }
+    }
+}
