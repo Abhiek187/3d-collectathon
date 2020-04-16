cba5079df8328aa864b96e9a517de2c9cf7f35ad
Abhiek187 <achaudhuri2011@yahoo.com>
2020-04-15 22:55:04 -0400

    Load scene async and show progress

--- Assets/Scripts/SceneScript.cs
+++ Assets/Scripts/SceneScript.cs
@@ -1,13 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneScript : MonoBehaviour
 {
-    public void SceneLoader(int sceneIndex)
+    [SerializeField] private TextMeshProUGUI progressText;
+
+    private void Start()
     {
-        // Transition to the given scene
-        SceneManager.LoadScene(sceneIndex);
+        if (progressText != null)
+        {
+            progressText.gameObject.SetActive(false); // hide progress until we load
+        }
+    }
+
+    // Only public void functions appear in the event trigger
+    public void SceneLoader(string scene)
+    {
+        if (progressText != null)
+        {
+            progressText.gameObject.SetActive(true);
+        }
+        
+        StartCoroutine(AsyncSceneLoader(scene));
+    }
+
+    private IEnumerator AsyncSceneLoader(string scene)
+    {
+        // Show loading progress and transition to the given scene
+        AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+
+        while (!ao.isDone)
+        {
+            if (progressText != null)
+            {
+                // ao.progress goes from 0-0.9, so clamp it from 0-1
+                float progress = Mathf.Clamp01(ao.progress / 0.9f);
+                progressText.SetText($"Loading: {progress * 100}%");
+            }
+            
+            yield return null;
+        }
     }
 }
