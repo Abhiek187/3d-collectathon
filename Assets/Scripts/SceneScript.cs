0efde6b0118745b116c4ec373e6905c0e570837b
Abhiek187 <achaudhuri2011@yahoo.com>
2020-04-15 21:19:08 -0400

    Create title and help scenes w/ buttons

---
+++ Assets/Scripts/SceneScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneScript : MonoBehaviour
+{
+    public void SceneLoader(int sceneIndex)
+    {
+        // Transition to the given scene
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
